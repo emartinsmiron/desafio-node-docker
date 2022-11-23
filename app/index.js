9bb30946f4a48304abdad3ae4b0e9fee868a6a6e
Emiron <emartinsmiron@gmail.com>
2022-11-23 00:51:22 -0300

    Commit do desafio full cycle

---
+++ app/index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const config = {
+    host: 'db', 
+    user: 'root', 
+    password: 'root',
+    database: 'nodedb'
+}
+
+
+app.get('/', (req, res) =>{
+    console.log('working');
+    const mysql = require('mysql')
+    const connection = mysql.createConnection(config)
+
+    const sql = `INSERT INTO people(name) values('Eduardo')`
+    connection.query(sql)
+
+    connection.query("SELECT * FROM people", function (err, result, fields) {
+        if (err) throw err;
+        
+        res.send(
+            `<h1>Full Cycle Rocks!</h1> 
+             <ol>
+                ${result.map(element => <li>${element.name}</li>)}
+            </ol>`
+            )
+      });
+    connection.end()
+})
+
+app.listen(port, () =>{
+    console.log('Rodando na porta ' + port)
+})
